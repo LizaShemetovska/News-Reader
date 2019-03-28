1ac57f3584064313c84c30739e5c7f1abcac8be1
shemetovskaliza <shemetovskaliza@A33COMP72>
2019-03-28 16:26:17 +0200

    add style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {NewsService} from './news.service';
+import { Source } from 'webpack-sources';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'news-reader';
+export class AppComponent implements OnInit{
+  mArticles: Array<any>;
+  mSources: Array<any>;
+ 
+ ngOnInit():void{
+   this.news.initArticles().subscribe(data => this.mArticles= data['articles']);
+   this.news.initSources().subscribe(data=>this.mSources=data['sources']);
+ }
+   searchArticles(source:string)
+   {
+     this.news.initArticlesById(source).subscribe(data=>this.mArticles=data['articles']);
+
+   }
+ constructor(private news: NewsService){}
 }
